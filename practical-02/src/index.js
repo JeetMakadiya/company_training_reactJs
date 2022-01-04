7ad31a8dc49e389afe655e1fc507385371475fa4
JeetMakadiya <jeetmakadiya01@gmail.com>
2022-01-04 19:03:00 +0530

    Dummy react project using webpack

---
+++ practical-02/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h1>Practical-02 Dummy react project using webpack</h1>,
+  document.getElementById('root')
+);
