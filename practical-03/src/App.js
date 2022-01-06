32fe7738775ac241a3b284fe8d6bbdc49857be21
JeetMakadiya <jeetmakadiya01@gmail.com>
2022-01-06 19:26:44 +0530

    React-Component-Approaches-Practical-03-Done-90%

--- practical-03/src/App.js
+++ practical-03/src/App.js
@@ -1,35 +1,85 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+const getLocalItems = () => {
+  let list = localStorage.getItem('lists');
+
+  if (list) {
+    return JSON.parse(localStorage.getItem('lists'));
+  } else {
+    return [];
+  }
+}
 
 function App() {
-  const [inputList, setInputList] = useState("");
-  const [Items, setItems] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [items, setItems] = useState(getLocalItems());
 
-  const itemEvent = (event) => {
-    setInputList(event.target.value);
+  let time = new Date().toLocaleTimeString();
+  let date = new Date().toDateString();
+  const [cTime, setCtime] = useState(time);
+  const [cDate, setCdate] = useState(date);
+  setInterval(() => {
+    time = new Date().toLocaleTimeString();
+    date = new Date().toDateString();
+    setCtime(time);
+    setCdate(date);
+  },1000);
+
+  const inputTextEvent = (event) => {
+    setInputValue(event.target.value);
   };
 
-  const listOfItems = () => {
-    setItems((oldItems) => {
-      return [...oldItems, inputList];
-    });
-    setInputList("");
+  const clickOnPlus = () => {
+    document.getElementById('textInput').style.display = 'inline-block';
+    document.getElementById('plusBtn').style.display = 'none';
   };
 
+  const addItem = (event) => {
+    if(event.keyCode === 13){
+      event.preventDefault();
+
+      if (inputValue != "") {
+        setItems((oldItems) => {
+          return [...oldItems, inputValue];
+        });
+        setInputValue("");
+      }else{
+        document.getElementById('errorMsg').innerText = "*Enter Something";
+      }
+    }
+  };
+
+  document.addEventListener('keydown', (event) => {
+    if (event.keyCode == 27 ) {
+      document.getElementById('textInput').style.display = 'none';
+      document.getElementById('plusBtn').style.display = 'inline-block';
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem('lists',JSON.stringify(items))}
+    ,[items]);
+  
   return (
     <>
-      <div class="todolist">
-        <h1>TODO LIST</h1>
-        <br />
-        <input type="text" value={inputList} placeholder="Add items" onChange={itemEvent} />
-        <button onClick={listOfItems}>+</button>
-        <ol>
-          {
-            Items.map((itemval) => {
-              return <li>{itemval}</li>;
-            })
-          }
-        </ol>
+      <div className="main">
+        <div className='date-time-div'>{cDate+" "+cTime}</div>
+        <div className='todolist-container'>
+          <div className='todolist-main'>
+            <h1>TODO LIST</h1>
+            <button onClick={clickOnPlus} id="plusBtn">+</button>
+            <ol>
+            {
+              items.map((itemval) => {
+                return <li>{itemval}</li>;
+              })
+            }
+            </ol>
+            <input type="text" id="textInput" value={inputValue} placeholder="Add items" onChange={inputTextEvent} onKeyUp={addItem}/>
+            <span className='errorMsg' id='errorMsg'></span>
+          </div>
+        </div>
       </div>
     </>
   );
